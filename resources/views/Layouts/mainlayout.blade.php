16f96d37135567aee33a0a02c5c906dd3a09d0a0
Akmal-Keisin <akmalkeisin@gmail.com>
2022-06-27 11:24:39 +0700

    fixing navbar layout

--- resources/views/Layouts/mainlayout.blade.php
+++ resources/views/Layouts/mainlayout.blade.php
@@ -96,7 +96,7 @@
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Session::get('auth')['data']['name'] }}</span>
                                 <img class="img-profile rounded-circle"
-                                    src="@if (Session::get('auth')['data']['image'] == null) {{ env('APP_URL') . '/images/default.png' }} @endif">
+                                    src="@if (Session::get('auth')['data']['image'] == null) {{ env('APP_URL') . '/images/default.png' }} @else {{ Session::get('auth')['data']['image'] }} @endif">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
